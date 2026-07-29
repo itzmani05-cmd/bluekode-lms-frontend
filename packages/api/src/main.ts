a79edd0b3233be36d2eb02f704620000192e3c71
itzmani-cmd <itzmani-cmd>
2026-07-29 19:06:29 +0530

    fix

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -6,8 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'https://bluekode-lms-frontend-vsvl.vercel.app',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
@@ -22,6 +24,6 @@ async function bootstrap() {
     SwaggerModule.createDocument(app, config),
   );
 
-  await app.listen(process.env.PORT ?? 5500);
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
