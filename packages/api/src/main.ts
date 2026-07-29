b0b30ad82768b41726c190aac78699f92be67cd2
itzmani-cmd <itzmani-cmd>
2026-07-29 18:41:16 +0530

    fixed: main.ts

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'https://bluekode-lms-frontend-qa2e.vercel.app/', credentials: true });
+  app.enableCors({ origin: 'https://bluekode-lms-frontend-qa2e.vercel.app', credentials: true });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
