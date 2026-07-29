703520eab681fc846015a15682dc5d6fef53fb4d
itzmani-cmd <itzmani-cmd>
2026-07-29 18:16:12 +0530

    fixing the cors

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
+  app.enableCors({ origin: 'https://bluekode-lms-frontend-qa2e.vercel.app/', credentials: true });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
